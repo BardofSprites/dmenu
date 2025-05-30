c09deddfe038b8c69fa3815b32ef209fff932bff
BardofSprites <89086143+BardofSprites@users.noreply.github.com>
2025-05-30 17:39:53 -0400

    added alpha patch at 0.9%

--- config.h
+++ config.h
@@ -5,9 +5,11 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;
 static int min_width = 800;
 static int border_width = 3;
+static const unsigned int alpha = 0xe6;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Comfy:pixelsize=18"
+	"Iosevka Comfy:pixelsize=18",
+    "NotoColorEmoji:pixelsize=18:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -16,6 +18,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 15;
 
